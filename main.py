c3483ad0622a964ceda01a2bc57150c3d8eb4445
Denis Krylov <krylov.den@icloud.com>
2023-12-28 13:37:33 +0100

    Add main.py and naive_strategy.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.strategies import naive_strategy as ns 
+
+
+if __name__ == "__main__":
+    ns.run_backtest()
--- src/strategies/naive_strategy.py
+++ src/strategies/naive_strategy.py
@@ -4,7 +4,6 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies import Strategy
 
 
-# A simple strategy that buys AAPL on the first day and hold it
 class MyStrategy(Strategy):
     def on_trading_iteration(self):
         if self.first_iteration:
@@ -13,18 +12,13 @@ class MyStrategy(Strategy):
             order = self.create_order("AAPL", quantity, "buy")
             self.submit_order(order)
 
-
-# Pick the dates that you want to start and end your backtest
-# and the allocated budget
-backtesting_start = datetime(2020, 11, 1, 0, 0, 0)
-backtesting_end = datetime(2020, 12, 31, 0, 0, 0)
-budget = 100000
-
-# Run the backtest
-MyStrategy.backtest(
-    "strat_1",
+def run_backtest(start=datetime(2020, 11, 1, 0, 0, 0), 
+                 end=datetime(2020, 12, 31, 0, 0, 0), 
+                 budget=100000):
+    MyStrategy.backtest(
+        "strat_1",
         budget,
         YahooDataBacktesting,
-        backtesting_start,
-        backtesting_end,
-)
+        start,
+        end,
+    )
