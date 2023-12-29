bd37ac56c7924b3abbe11280f70f8bd4730455c5
Denis Krylov <krylov.den@icloud.com>
2023-12-29 01:19:38 +0100

    Refactor profile and strategies

--- src/profile/profile.py
+++
@@ -1,5 +0,0 @@
-class Profile:
-
-    def __init__(self, name, budget) -> None:
-        self.name = name
-        self.budget = budget
--- src/strategies/naive_strategy.py
+++ src/strategies/naive_strategy.py
@@ -1,10 +1,14 @@
 from datetime import datetime
-
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies import Strategy
 
 
-class MyStrategy(Strategy):
+class NaiveStrategy(Strategy):
+
+    def initialize(self):
+        self.sleeptime = "10M"
+        self.minutes_before_closing = 15
+
     def on_trading_iteration(self):
         if self.first_iteration:
             aapl_price = self.get_last_price("AAPL")
@@ -12,10 +16,11 @@ class MyStrategy(Strategy):
             order = self.create_order("AAPL", quantity, "buy")
             self.submit_order(order)
 
+
 def run_backtest(start=datetime(2020, 11, 1, 0, 0, 0), 
                  end=datetime(2020, 12, 31, 0, 0, 0), 
                  budget=100000):
-    MyStrategy.backtest(
+    NaiveStrategy.backtest(
         "strat_1",
         budget,
         YahooDataBacktesting,
