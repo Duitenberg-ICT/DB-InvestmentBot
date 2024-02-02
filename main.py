6ddd963649b1b1c747bae46e7204eed939d19b63
Aaron <y.chen-19@student.utwente.nl>
2024-02-02 22:49:28 +0800

    made slight tweaks and added todos

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from src.strategies import benchmark as ns
+import yfinance as yf
+import datetime
 
+tickers = ['AAPL', 'GOOG', 'AMZN', 'NVDA', 'META', 'TSLA', 'MSFT', 'ORCL', 'AMD', 'INTC']
+start = datetime.datetime(2020, 1, 1)
+end = datetime.datetime(2023, 12, 31)
 
-if __name__ == "__main__":
-    pass
+for ticker in tickers:
+    price_data = yf.download(tickers=ticker, period="5d", interval="15m")
+    # filtered = price_data[start: end]["Close"].tolist()
+    # print(filtered)
--- src/strategies/big_tech_MR.py
+++ src/strategies/big_tech_MR.py
@@ -9,7 +9,12 @@ class MeanReversion_tech(Strategy):
     def initialize(self):
         self.sleeptime = "1D"
         self.symbols = self.get_sp500_symbols()
-        self.big_players = ['AAPL', 'GOOG', 'AMZN', 'NVDA', 'META', 'TSLA', 'MSFT', 'ORCL', 'AMD', 'INTC']
+        self.big_tech = ['AAPL', 'GOOG', 'AVGO', 'NVDA', 'META', 'TSLA', 'MSFT', 'ORCL', 'AMD', 'INTC']
+        # self.big_finance = ['BRK-B', 'JPM', 'V', 'MA', 'BAC', 'GS', 'WFC', 'C', 'AXP', 'PYPL']
+        # self.big_natural_resources = ['XOM', 'CVX', 'COP', 'LIN', 'EOG', 'NEE', 'GE', 'DUK', 'SLB', 'SO']
+        # self.big_health = ['LLY', 'JNJ', 'ABBV', 'PFE', 'AMGN', 'MRK', 'BMY', 'GILD', 'UNH', 'ABT']
+        self.big_consumer = ['AMZN', 'HD', 'MCD', 'BKNG', 'NKE', 'KO', 'PG', 'WMT', 'COST', 'PEP']
+        self.picks = self.pick_symbols()
 
     def get_sp500_symbols(self):
         table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -19,36 +24,42 @@ class MeanReversion_tech(Strategy):
         symbols = [symbol.replace('.', '-') for symbol in symbols]
         return symbols
 
+    def pick_symbols(self):
+        return self.big_tech + self.big_consumer
+        # return self.big_tech + self.big_finance + self.big_natural_resources + self.big_health + self.big_consumer
+
+    # todo implement sell when position falls by x percent
+    # todo allocate dynamically budget for each stock based on market share / most below MA
     def on_trading_iteration(self):
         cash_available = self.cash
-        per_stock_allocation = cash_available / len(self.big_players)
+        per_stock_allocation = cash_available / len(self.picks)
 
-        for ticker in self.big_players:
-            bars = self.get_historical_prices(ticker, 30, "day")
+        for ticker in self.picks:
+            bars = self.get_historical_prices(ticker, 35, "day")
             prices = bars.df['close'].values
             mean = prices.mean()
             std_dev = prices.std()
             latest_close = prices[-1]
 
-            print("cash available: " + str(cash_available))
-
             if latest_close > mean + std_dev and self.get_position(ticker) is not None:
                 order = self.create_order(ticker, self.get_position(ticker).quantity, 'sell')
                 self.submit_order(order)
                 print('selling ' + str(self.get_position(ticker).quantity) + ' shares of ' + ticker + ' for ' + str(latest_close))
+                print("cash available: " + str(cash_available))
             elif latest_close < mean - std_dev:
                 if per_stock_allocation >= self.get_last_price(ticker):
                     order = self.create_order(ticker, per_stock_allocation // self.get_last_price(ticker), 'buy')
                     self.submit_order(order)
                     print('buying ' + str(per_stock_allocation // self.get_last_price(ticker)) + ' shares of ' + ticker + ' for ' + str(
                         self.get_last_price(ticker)))
+                    print("cash available: " + str(cash_available))
 
     def on_backtest_end(self):
         self.log_returns()
 
 
-backtesting_start = datetime.datetime(2020, 1, 1)
-backtesting_end = datetime.datetime(2023, 12, 31)
+backtesting_start = datetime.datetime(2023, 2, 1)
+backtesting_end = datetime.datetime(2024, 2, 1)
 MeanReversion_tech.backtest(
     YahooDataBacktesting,
     backtesting_start,
